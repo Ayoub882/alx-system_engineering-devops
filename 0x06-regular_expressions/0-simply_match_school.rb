bf2f008ee5e8a399100351f5dbd8959ac5e93a3f
Ayoub882 <ayoubrguig882@gmail.com>
2024-05-09 14:36:58 +0100

    +

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,15 +1,3 @@
 #!/usr/bin/env ruby
-
-# Extract the first argument as the input string
-input_string = ARGV[0]
-
-# Define the regular expression to match "School"
-regexp = /School/
-
-# Find all occurrences of "School" in the input string
-matches = input_string.scan(regexp)
-
-# Print each match followed by a dollar sign
-matches.each do |match|
-  puts "#{match}$"
-end
+# test
+puts ARGV[0].scan(/School/).join
